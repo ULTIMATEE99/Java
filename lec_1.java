de91c52082c5c64b93b1d1e5fbc8c92db184b020
ULTIMATEE99 <ultimatee9999@gmail.com>
2026-08-17 14:22:14 +0530

    adding comments

---
+++ lec_1.java
@@ -0,0 +1,15 @@
+public class lec_1 {
+    public static void main(String[] args) {
+        System.out.println("Starting Java journey!");
+        System.out.println("teseting name change!");
+    }
+}
+
+//lec_1 is class name 
+//public used becuase the main function can be used outside 
+// static becuase to use method withoput creating object of it because JVM needs an entry point
+// javac and complie the .java to .class 
+//string[] args stroe command line arguments
+//javac -d . lec_1.java  this will make .class file in previous directory (used in terminal)
+//javac -d .. lec_1.java  this will make .class file in previous to previous directory (used in terminal)
+//javac lec_1.java will make class in the same directory (used in terimnal)
