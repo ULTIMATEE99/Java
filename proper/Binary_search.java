22ba7892d7e32db8bcf009feb5f4ec8951a83638
ULTIMATEE99 <ultimatee9999@gmail.com>
2026-08-20 09:24:35 +0530

    Binary search

---
+++ proper/Binary_search.java
@@ -0,0 +1,19 @@
+package proper;
+
+import java.util.Arrays;
+
+public class Binary_search {
+
+    public static void main(String[] args) {
+
+        // Java inbuilt binary search using Arrays class
+        // Arrays.binarySearch(arrayName, targetKey)
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 30;
+
+        int result = Arrays.binarySearch(arr, target);
+
+        System.out.println("Element found at position: " + (result + 1));
+    }
+}
