e7fc40f1403f28dbd446c2b0b5f036c957166728
ULTIMATEE99 <ultimatee9999@gmail.com>
2026-08-22 15:45:02 +0530

    test case passing wihout this

--- Hackerrank.java
+++ Hackerrank.java
@@ -3,7 +3,7 @@ import java.util.*;
 abstract class Book {
     String title;
 
-    abstract void setTitle(String s);
+    //abstract void setTitle(String s);
 
     String getTitle() {
         return title;
