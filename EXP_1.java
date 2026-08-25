a6097c007d2f33a3ad90351611fdd2efd79f0d07
ULTIMATEE99 <ultimatee9999@gmail.com>
2026-08-25 13:22:15 +0530

    EXP 1.1

---
+++ EXP_1.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+class EXP_1{
+
+    static boolean isPowerOfTwo(int n) {
+
+        // Base case
+        if (n == 1) {
+            return true;
+        }
+
+        // Base case
+        if (n <= 0 || n % 2 != 0) {
+            return false;
+        }
+
+        // Recursive case
+        return isPowerOfTwo(n / 2);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean result = isPowerOfTwo(n);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
